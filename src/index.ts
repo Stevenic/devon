208cdad363c8da70d9e1b5f058d9c414f34ac182
Steven Ickman <stevenic@microsoft.com>
2019-01-03 11:17:18 -0800

    Added quit command

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ import { ConsoleAdapter } from './consoleAdapter';
 import chalk from 'chalk';
 
 const adapter = new ConsoleAdapter().listen(async (context) => {
-    await context.sendActivity({ type: ActivityTypes.Typing });
-    await context.sendActivity({ type: 'delay', value: 5000 });
-    await context.sendActivity(chalk.blue(`you said: ` + context.activity.text));
+    if (context.activity.text === 'quit') {
+        process.exit();
+    } else {
+        await context.sendActivity({ type: ActivityTypes.Typing });
+        await context.sendActivity({ type: 'delay', value: 5000 });
+        await context.sendActivity(chalk.blue(`you said: ` + context.activity.text));
+    }
 });
 
