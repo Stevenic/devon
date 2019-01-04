484c6092496c557f9ceafd4bf00f0426e502aff4
Hai Liu <hailiu@gmail.com>
2019-01-03 17:32:50 -0800

    check test failure case

--- src/spawnCmd.spec.ts
+++ src/spawnCmd.spec.ts
@@ -1,17 +1,20 @@
 ///<reference types='jasmine'/>
 import { spawnCmd } from './spawnCmd';
 
-describe('executeCmd', async () => {
+describe('executeCmd', () => {
     it('parse output json', async () => {
         const names = await spawnCmd<string[]>('az account list --query [*].name');
         expect(names.length).toBeGreaterThan(1);
     });
+
     it('rejects on invalid command', async () => {
+        let error = null;
         try {
-            await spawnCmd<string[]>('az account bogus --query [*].name');
-            expect('should not got here').toBe(null);
+            const names = await spawnCmd<string[]>('az account bogus --query [*].name');
+            expect('should not got here').toEqual(null);
         } catch (ex) {
-            expect(ex).not.toBeNull();
+            error = ex;
         }
+        expect(error).not.toBeNull();
     });
 });
